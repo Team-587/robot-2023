832acedf459a649c5126fc89b7da8c17108ba7d7
hedgehogs@team587 <hedgehogs@team587.org>
2023-02-13 19:30:20 -0500

    add spare spark id

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -54,6 +54,8 @@ constexpr int kIntakeMotor = 30;
 constexpr int kElevatorMotor1 = 31;
 constexpr int kElevatorMotor2 = 32;
 
+constexpr int kSpareMotor = 33;
+
 constexpr int kFrontLeftTurningEncoderPorts[2]{0, 1};
 constexpr int kRearLeftTurningEncoderPorts[2]{2, 3};
 constexpr int kFrontRightTurningEncoderPorts[2]{4, 5};
